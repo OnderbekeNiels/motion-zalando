9514205703ae5ed6b789710e4365ff20b94b3683
OnderbekeNiels <niels.onderbeke@student.howest.be>
2021-04-10 22:32:47 +0200

    timing animations

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,21 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Fade from 'react-reveal/Fade';
 import Lottie from 'react-lottie';
 
 export default function Card(props: any) {
 
-    const [pause, setPause] = useState<boolean>(true);
-
-    const showAnimation = () =>{
-        setTimeout(() =>{
-            
-        },2000)
-        setPause(false);
-    }
+    const [isStarted, setIsStarted] = useState<boolean>(true);
 
     const defaultOptions = {
-        loop: false,
-        autoPlay: false,
+        loop: true,
+        autoplay: false,
         animationData: props.animation,
         rendererSettings: {
             preserveAspectRatio: "xMidYMid slice"
@@ -23,17 +16,16 @@ export default function Card(props: any) {
     };
 
     return (
-        <Fade onReveal={() => showAnimation} bottom duration={800}><article className='c-card' >
+        <Fade onReveal={() => setIsStarted(false)} wait={1300} bottom duration={800}><article className='c-card' >
             <Lottie
                 options={defaultOptions}
                 height={200}
                 width={200}
-                isStopped={pause} />
+                isPaused={isStarted} />
             <h3 className='c-card__title u-max-width--xs'>{props.title}</h3>
             <p>
                 {props.description}
             </p>
         </article></Fade>
-
     )
 }
