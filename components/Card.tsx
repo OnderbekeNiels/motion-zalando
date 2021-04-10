cfa79b62f8331d59cbcd372981fcc6aa6c8b7ea5
OnderbekeNiels <niels.onderbeke@student.howest.be>
2021-04-10 12:14:52 +0200

    features animations update

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import Lottie from 'react-lottie';
 
 export default function Card(props: any) {
 
+    const [pause, setPause] = useState<boolean>(false);
+
+    setTimeout(function () { setPause(!pause); }, 3000);
+
     const defaultOptions = {
-        loop: false,
-        autoplay: true,
+        loop:false,
         animationData: props.animation,
         rendererSettings: {
             preserveAspectRatio: "xMidYMid slice"
@@ -16,7 +20,8 @@ export default function Card(props: any) {
             <Lottie
                 options={defaultOptions}
                 height={200}
-                width={200} />
+                width={200}
+                isStopped={pause} />
             <h3 className='c-card__title u-max-width--xs'>{props.title}</h3>
             <p>
                 {props.description}
