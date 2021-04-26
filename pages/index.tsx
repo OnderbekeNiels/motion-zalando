88bb688d022f0c4c9f2160d5c943517a347c4409
OnderbekeNiels <niels.onderbeke@student.howest.be>
2021-04-26 23:48:44 +0200

    horizontal animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import Footer from '../components/Footer';
 import DeliveryServiceIcon from '../components/DeliveryServiceIcon';
 import heartPop from '../lotties/heartPop.json';
 import clothesCarousel from '../lotties/clothesCarousel.json';
+import clothesXCarousel from '../lotties/clothesXCarousel.json';
 import card from '../lotties/card.json';
 import Screencast from '../components/Screencast';
 import Menu from '../components/Menu';
@@ -95,7 +96,7 @@ export default function Home() {
           <Row className='u-bg-color-accents--yellow-250'>
               <Container className='c-card-container'>
                 <Card title='Bewaar de artikelen die je leuk vindt.' description='Geef de artikelen die jou aantrekken een hartje & vindt deze steeds terug na het scrollen door ons assortiment.' animation={heartPop} />
-                <Card title='Nieuw kledingstijl ? Help! ' description='Scroll door vele verschillende stijlen en vindt de look die bij jou past! Zalando sorteert ze alvast voor jou.' animation={clothesCarousel} />
+                <Card title='Nieuw kledingstijl ? Help! ' description='Scroll door vele verschillende stijlen en vindt de look die bij jou past! Zalando sorteert ze alvast voor jou.' animation={clothesXCarousel} />
                 <Card title='Betalen? Mogelijkheden genoeg!' description='Betalen bij zalando is een fluitje van een cent. Betaal met Bancontact, Paypal, Meastro, Mastercard, Visa, Amex of gewoonweg met een overschrijving.' animation={card} />
               </Container>
           </Row>
