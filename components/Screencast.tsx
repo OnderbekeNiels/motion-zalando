0a644c7899c253ffdc023c461e24c932d2f50505
OnderbekeNiels <niels.onderbeke@student.howest.be>
2021-04-30 12:02:50 +0200

    fixed loose animation

--- components/Screencast.tsx
+++ components/Screencast.tsx
@@ -1,7 +1,7 @@
 export default function Screencast() {
     return (
         <video className='c-screencast' autoPlay loop muted controls poster="/img/png/thumbnail.png">
-                <source src="/video/screencastFeatures_Final.mp4" type="video/mp4"/>
-            </video>
+            <source src="/video/screencastFeatures_Final.mp4" type="video/mp4" />
+        </video>
     )
 }
